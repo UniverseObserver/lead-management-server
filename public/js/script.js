55a4eeed2985918492b2851995beda6fe80006f1
AOIDUO <admin@GENESIS.org>
2017-12-28 16:57:56 +0800

    angular-patternfly navigation

--- public/js/script.js
+++ public/js/script.js
@@ -2,15 +2,20 @@ angular.module('patternfly.navigation').controller('vertNavController', ['$scope
     function ($scope) {
         $scope.navigationItems = [
             {
-                title: "Contacts",
+                title: "item1",
                 iconClass: "fa fa-user",
-                href: "#/contacts"
+                href: "#/item1"
             },
             {
-                title: "Groups",
+                title: "item2",
                 iconClass: "fa fa-users",
-                href: "#/groups"
-            }
+                href: "item2"
+            },
+			{
+				title: "item3",
+				iconClass: "fa fa-user",
+				herf: "item3"
+			}
         ];
     }
 ]);
